f7450e7d23d3c42de410cfc546fd4e46c9953c1e
batibot323 <hani.herbert.ho@gmail.com>
2019-12-11 22:08:56 +0800

    Copied code from Checkboxes: https://www.telerik.com/kendo-angular-ui/components/treeview/checkboxes/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,119 @@
 import { Component } from '@angular/core';
+import { CheckableSettings } from '@progress/kendo-angular-treeview';
+import { of, Observable } from 'rxjs';
+
 @Component({
     selector: 'app-root',
+    styles: [`.right { margin-right: 5px }`],
     template: `
-    <kendo-treeview
-        [nodes]="data"
-        textField="text"
-        kendoTreeViewExpandable
-        kendoTreeViewSelectable
-        kendoTreeViewHierarchyBinding
-        childrenField="items"
-    >
-    </kendo-treeview>
+        <fieldset>
+            <legend>Check Settings</legend>
+            <div class="k-form-field" style="margin-bottom: 1em;">
+                <span class="right"><strong>Mode:</strong></span>
+                <input type="radio" name="checkMode" id="singleCheck" value="single" class="k-radio right"
+                    [(ngModel)]="checkMode" />
+                <label class="k-radio-label right" for="singleCheck">Single</label>
+                <input type="radio" name="checkMode" id="multipleCheck" value="multiple" class="k-radio"
+                    [(ngModel)]="checkMode" />
+                <label class="k-radio-label" for="multipleCheck">Multiple</label>
+            </div>
+            <div  *ngIf="checkMode == 'multiple'">
+                <label class="k-form-field right">
+                    <input
+                        type="checkbox"
+                        id="enableCheck"
+                        class="k-checkbox"
+                        [(ngModel)]="enableCheck"
+                    />
+                    <label class="k-checkbox-label" for="enableCheck">Enable</label>
+                </label>
+                <label class="k-form-field right">
+                    <input
+                        type="checkbox"
+                        id="checkChildren"
+                        class="k-checkbox"
+                        [(ngModel)]="checkChildren"
+                    />
+                    <label class="k-checkbox-label" for="checkChildren">Check all children</label>
+                </label>
+                <label class="k-form-field right">
+                    <input
+                        type="checkbox"
+                        id="checkParents"
+                        class="k-checkbox"
+                        [(ngModel)]="checkParents"
+                    />
+                    <label class="k-checkbox-label" for="checkParents">Check all parents when children are checked</label>
+                </label>
+                <label class="k-form-field right">
+                    <input
+                        type="checkbox"
+                        id="checkOnClick"
+                        class="k-checkbox"
+                        [(ngModel)]="checkOnClick"
+                    />
+                    <label class="k-checkbox-label" for="checkOnClick">Check the node on click</label>
+                </label>
+            </div>
+        </fieldset>
+        <br />
+        <kendo-treeview
+            [nodes]="data"
+            [children]="children"
+            [hasChildren]="hasChildren"
+            textField="text"
+
+            [kendoTreeViewCheckable]="checkableSettings"
+            [(checkedKeys)]="checkedKeys"
+        >
+        </kendo-treeview>
+        <div style="margin: 10px 0">
+            <i>Press SPACE to check/uncheck the active node</i>
+            <div class="example-config">
+                Checked keys: {{checkedKeys.join(",")}}
+            </div>
+        </div>
   `
 })
 export class AppComponent {
+    public checkedKeys: any[] = ['1'];
+
+    public enableCheck = true;
+    public checkChildren = true;
+    public checkParents = true;
+    public checkOnClick = false;
+    public checkMode: any = 'multiple';
+    public selectionMode: any = 'single';
+
+    public get checkableSettings(): CheckableSettings {
+        return {
+            checkChildren: this.checkChildren,
+            checkParents: this.checkParents,
+            enabled: this.enableCheck,
+            mode: this.checkMode,
+            checkOnClick: this.checkOnClick
+        };
+    }
+
     public data: any[] = [
         {
-            text: 'Furniture', items: [
-                { text: 'Tables & Chairs' },
-                { text: 'Sofas' },
-                { text: 'Occasional Furniture' }
-            ]
+          text: 'Furniture', items: [
+            { text: 'Tables & Chairs' },
+            { text: 'Sofas' },
+            {
+              text: 'Occasional Furniture', items: [{
+                text: 'Decor', items: [
+                  { text: 'Bed Linen' },
+                  { text: 'Curtains & Blinds' }
+                ]
+              }]
+            }
+          ]
         },
-        {
-            text: 'Decor', items: [
-                { text: 'Bed Linen' },
-                { text: 'Curtains & Blinds' },
-                { text: 'Carpets' }
-            ]
-        }
+        { text: 'Decor' },
+        { text: 'Outdoors' }
     ];
-}
 
+    public children = (dataItem: any): Observable<any[]> => of(dataItem.items);
+    public hasChildren = (dataItem: any): boolean => !!dataItem.items;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { TreeViewModule } from '@progress/kendo-angular-treeview';
 
 import { AppComponent } from './app.component';
@@ -8,7 +9,6 @@ import { AppComponent } from './app.component';
 @NgModule({
   bootstrap:    [ AppComponent ],
   declarations: [ AppComponent ],
-  imports:      [ BrowserModule, BrowserAnimationsModule, TreeViewModule]
+  imports:      [ BrowserModule, BrowserAnimationsModule, TreeViewModule, FormsModule]
 })
 export class AppModule { }
-
