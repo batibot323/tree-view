bb6c9101359ea692398772e7920e0256c352a1e3
batibot323 <hani.herbert.ho@gmail.com>
2019-12-11 22:07:26 +0800

    Copied code Getting Started TreeView: https://www.telerik.com/kendo-angular-ui/components/treeview/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+    <kendo-treeview
+        [nodes]="data"
+        textField="text"
+        kendoTreeViewExpandable
+        kendoTreeViewSelectable
+        kendoTreeViewHierarchyBinding
+        childrenField="items"
+    >
+    </kendo-treeview>
+  `
 })
 export class AppComponent {
-  title = 'tree-view';
+    public data: any[] = [
+        {
+            text: 'Furniture', items: [
+                { text: 'Tables & Chairs' },
+                { text: 'Sofas' },
+                { text: 'Occasional Furniture' }
+            ]
+        },
+        {
+            text: 'Decor', items: [
+                { text: 'Bed Linen' },
+                { text: 'Curtains & Blinds' },
+                { text: 'Carpets' }
+            ]
+        }
+    ];
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
 
 import { AppComponent } from './app.component';
-import { TreeViewModule } from '@progress/kendo-angular-treeview';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    TreeViewModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:    [ AppComponent ],
+  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, TreeViewModule]
 })
 export class AppModule { }
+
