28cce04a0aac2e066aa2f936e4245acb07710d8e
batibot323 <hani.herbert.ho@gmail.com>
2019-12-11 22:04:41 +0800

    Initial commit. Installed Kendo UI TreeView.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeViewModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
